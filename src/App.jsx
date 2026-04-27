18dd83783cfdbc00e9c7db1003bd12b2eb725953
riceball <lten10000@gmai.com>
2026-04-27 13:00:09 +0900

    react

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import './App.css'
 import { Header } from './components/Header';
 import { MainContent } from './components/MainContent';
 import { Footer } from './components/Footer';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { ThemeProvider } from './components/ThemeProvider';
 
 function App() {
   const siteTitle = 'Reactの基礎を学ぼう';
@@ -11,12 +12,17 @@ function App() {
   // 進捗率をstateとして管理
   const [progress, setProgress] = useState(0); // 初期値は0％
 
+    // ブラウザ上部のタイトルを変更
+  useEffect(() => {
+    document.title = siteTitle;
+  }, []); // コンポーネントの初回描画時にのみ実行
+
   return (
-    <>
+     <ThemeProvider>
       <Header siteTitle={siteTitle} />
       <MainContent progress={progress} setProgress={setProgress} />
       <Footer siteTitle={siteTitle} currentYear={currentYear} />
-    </>
+     </ThemeProvider>
   )
 }
 
